ec409eb566f0503e47bf1f163c287fa6098eb35b
Ashutosh Jha <yesaj9354@gamil.com>
2024-01-19 12:41:44 +0530

    commiing due to build failed

--- next.config.js
+++ next.config.js
@@ -1,15 +1,7 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 const nextConfig = {
-    async generateStaticParams() {
-        // Your custom logic to generate static params
-        return {
-          '/': { page: '/' },
-          '/login': { page: '/login' },
-          '/feed': { page: '/feed' },
-          // Add more paths as needed
-        };
-      },
+    output:"export",
     images:{unoptimized:true},
     sassOptions:{
         includePaths: [path.join(__dirname,'styles')]
