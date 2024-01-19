322ee3232931e156b23c79ca0844d81705208967
Ashutosh Jha <yesaj9354@gamil.com>
2024-01-19 11:55:02 +0530

    experimental commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const path = require('path');
 const nextConfig = {
-    // output:"export",
+    output:"export",
     images:{unoptimized:true},
     sassOptions:{
         includePaths: [path.join(__dirname,'styles')]
